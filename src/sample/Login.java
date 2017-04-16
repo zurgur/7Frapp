a7cba3b7533d8ee80fb723dda33100089b4d9f7b
zurgur <harryfreyr@gmail.com>
2017-04-16 08:22:20 +0000

    flug fram og til baka

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -1,20 +1,16 @@
 package sample;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXPasswordField;
-import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
-import java.util.ResourceBundle;
 
 /**
  * Created by Alexander on 4/11/2017.
@@ -23,19 +19,19 @@ public class Login{
     //búa til FXML hluti svo við getum notað þá ur skjalinu
 
     @FXML
-    private JFXTextField user;
+    private TextField user;
 
     @FXML
-    private JFXPasswordField password;
+    private PasswordField password;
 
     @FXML
-    private JFXButton login;
+    private Button login;
 
     @FXML
-    private JFXButton signup;
+    private Button signup;
 
     @FXML
-    private JFXButton goback;
+    private Button goback;
 
     @FXML
     void makeLogin(ActionEvent event)
--- src/sample/Search.java
+++ src/sample/Search.java
@@ -7,7 +7,6 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
-import sample.tengingVidGagnagrunn.*;
 
 /**
  * Created by Alexander on 7.4.2017.
@@ -28,30 +27,55 @@ public class Search {
     RadioButton hadeigi;
     @FXML
     RadioButton kvöld;
+    @FXML
+    DatePicker date2;
 
     tengingVidGagnagrunn t = new tengingVidGagnagrunn();
     ArrayList<String> fra = t.getFrom();
     ArrayList<String> til = t.getTo();
     ArrayList<String> data = t.getDate();
     ArrayList<String> time = t.getTime();
+    Boolean timi1Valin = false;
+    Boolean timi2Valin = false;
 
     public void leitaAction(ActionEvent actionEvent){
+        if(timi1Valin && timi2Valin){
+            backAndForth();
+        } else if(timi1Valin){
+            oneWay();
+        }
+
+    }
+
+    private void oneWay() {
         String departurText = departur.getText();
         String arivalText = arival.getText();
         String dateText = (date.getValue()).toString();
-        System.out.println(dateText);
         ArrayList<String> tilEr = searchForFlight(departurText,arivalText,dateText);
+        System.out.println("one way flug:");
         System.out.println(tilEr);
-        if (morgun.isSelected()){
-            System.out.println("morgun");
-        }else if(hadeigi.isSelected()){
-            System.out.println("hádeigis");
-        }else if(kvöld.isSelected()){
-            System.out.println("kvöld");
-        }
-
-
     }
+
+    private void backAndForth() {
+        String departurText = departur.getText();
+        String arivalText = arival.getText();
+        String dateText = (date.getValue()).toString();
+        String date2Text = (date2.getValue()).toString();
+        System.out.println("flug útt");
+        ArrayList<String> utt = searchForFlight(departurText, arivalText, dateText);
+        System.out.println(utt);
+        System.out.println("flug heim:");
+        ArrayList<String> heim = searchForFlight(arivalText,departurText,date2Text);
+        System.out.println(heim);
+    }
+
+    public void setTimi1Action(ActionEvent actionEvent){
+        timi1Valin = true;
+    }
+    public void setTimi2Action(ActionEvent actionEvent){
+        timi2Valin = true;
+    }
+
     public ArrayList<String> searchForFlight(String departure, String arrival,String date){
         //prufa verður að hafa try og catch fyrir SQLite tengniguna
         // fer í gegn um arry-ana
@@ -59,8 +83,6 @@ public class Search {
         for(int i = 0; i<fra.size();i++){
             String Str = time.get(0).substring(0,2);
             int tim = Integer.parseInt(Str);
-            System.out.println(tim);
-
             if (tim<=10 && morgun.isSelected() && (fra.get(i)).equalsIgnoreCase(departure)&&
                     (til.get(i)).equalsIgnoreCase(arrival) && (data.get(i)).equals(date)){
                 fraTil.add(departure +" "+ arrival + " " + data.get(i) + " " + time.get(i));
@@ -77,8 +99,6 @@ public class Search {
                     && (data.get(i)).equals(date)){
                 fraTil.add(departure +" "+ arrival + " " + data.get(i) + " " + time.get(i));
             }
-
-
         }
         return fraTil;
     }
