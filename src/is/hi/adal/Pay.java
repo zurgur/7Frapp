a3b5d09b6257aaacc0d63a9b9ec6817d8dcf480e
Sunna <sunnads@hotmail.com>
2017-04-18 11:52:40 +0000

    Athuga hvort eitthvað sem ég geri virki

--- src/is/hi/adal/Pay.java
+++ src/is/hi/adal/Pay.java
@@ -1,14 +1,39 @@
 package is.hi.adal;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * Created by Sigfús on 14.4.2017.
  */
 public class Pay {
-    public void LoginAction(ActionEvent actionEvent) {
-    }
 
-    /* bara að prufa hvort eitthvað sem ég geri virki :) :) :) */
+    @FXML
+    private Button pay;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField gender;
+    @FXML
+    private TextField birthday;
+    @FXML
+    private TextField nationality;
+    @FXML
+    private TextField cardholder;
+    @FXML
+    private TextField cardNumber;
+    @FXML
+    private TextField cvc;
+    @FXML
+    private Button back;
+
+
+
+
+    
 
 }
