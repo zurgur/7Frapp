d8fb0f85ca624c9e6f04b7ce1e1eb55da8cf58f1
Sunna <sunnads@hotmail.com>
2017-04-20 18:33:16 +0000

    verið að bæta öllu aftur við, því allir kóði hvarf

---
+++ src/is/hi/adal/Flight.java
@@ -0,0 +1,59 @@
+package is.hi.adal;
+
+/**
+ * Created by Alexander on 20.4.2017.
+ */
+public class Flight {
+    private String date;
+    private String destinasion;
+    private String from;
+    private int cost;
+    private int seats;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDestinasion() {
+        return destinasion;
+    }
+
+    public void setDestinasion(String destinasion) {
+        this.destinasion = destinasion;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+    public Flight(String dagur, String afangastadur, String fra, int kostnaður, int Seats){
+        this.date = dagur;
+        this.destinasion = afangastadur;
+        this.from = fra;
+        this.cost = kostnaður;
+        this.seats = Seats;
+    }
+}
